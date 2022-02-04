43efa29030f470a467d29c0556ad1c397d8153e9
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-02-05 03:17:36 +0530

    Update settings.py

--- rockpaperscissors/settings.py
+++ rockpaperscissors/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)7d2+)sff!f(4t7t_!&58pmbzq8@eb98p6zp*khl56$lk39kvd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rock--paper---scissors.herokuapp.com']
 
 
 # Application definition
@@ -119,5 +119,13 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Activate Django-Heroku.
+# django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
